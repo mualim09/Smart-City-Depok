d1c46d36bce0b2a326116c12bf6bd7bbc103c1a4
wordwar <wordwar@github,com>
2018-07-15 22:34:03 +0700

    asd
    asd

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,7 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'willvincent\\Feeds\\' => array($vendorDir . '/willvincent/feeds/src'),
-    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/type-resolver/src', $vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/reflection-docblock/src', $vendorDir . '/phpdocumentor/type-resolver/src'),
     'XdgBaseDir\\' => array($vendorDir . '/dnoegel/php-xdg-base-dir/src'),
     'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
     'Unisharp\\Ckeditor\\' => array($vendorDir . '/unisharp/laravel-ckeditor'),
